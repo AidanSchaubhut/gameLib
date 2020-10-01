03b12f0c3dfe2f24ac400e6ba7d5d692503d36d9
AidanSchaubhut <aidanjs1208@gmail.com>
2020-10-01 23:23:17 +0000

    * added getAngleDegrees()

--- index.js
+++ index.js
@@ -14,6 +14,22 @@
         radiansToDegrees(radians){
             return radians * 180 /Math.PI;
         },
+
+        getAngleDegrees(pointA, pointB){
+            const 
+                distanceX = pointB.x - pointA.x,
+                distanceY = pointB.y - pointA.y,
+                radians = Math.atan2(distanceY, distanceX),
+                degrees =  radians * 180 / Math.PI;
+            return degrees;
+        },
+
+        degreesToRadians(degrees){
+            return degrees * Math.PI / 180;
+        },
+        radiansToDegrees(radians){
+            return radians *180 / Math.PI
+        },
       
 },
     phyz: {
